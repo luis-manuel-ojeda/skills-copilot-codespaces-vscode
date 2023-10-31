ad6604d9a97a41b3bdec92378c5cdb880debd75a
Luis Ojeda <116924503+luis-manuel-ojeda@users.noreply.github.com>
2023-10-31 20:48:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills/member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMemberCtrl',
+    bindToController: true,
+    scope: {
+      member: '='
+    }
+  };
+}
